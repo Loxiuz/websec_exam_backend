07409e67ac964dabffadb017c3a13aa2e4e593f0
Malte Bjoerklund <maltebjoerklund@gmail.com>
2026-02-15 21:09:09 +0100

    disable X-XSS-Protection

--- src/main/java/com/websec_exam_backend/security/SpringSecurityConfig.java
+++ src/main/java/com/websec_exam_backend/security/SpringSecurityConfig.java
@@ -31,7 +31,7 @@ public class SpringSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
         http.csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.xssProtection(HeadersConfigurer.XXssConfig::disable)) //X-XSS-Protection is deprecated
+                .headers(headers -> headers.xssProtection(xXssConfig -> xXssConfig.disable())) //X-XSS-Protection is deprecated
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/auth/**").permitAll();
                     authorize.anyRequest().authenticated();
